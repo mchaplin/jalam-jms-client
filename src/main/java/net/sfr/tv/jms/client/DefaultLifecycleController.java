88e321adba3f48e42d63c34ae321b6fca446f0a0
pierrecdn <me@pierre-cheynier.net>
2014-03-05 19:31:40 +0100

    - Merge with current rev.

--- src/main/java/net/sfr/tv/jms/client/DefaultLifecycleController.java
+++ src/main/java/net/sfr/tv/jms/client/DefaultLifecycleController.java
@@ -30,7 +30,7 @@ import org.apache.log4j.Logger;
  * @see net.sfr.tv.jms.client.api.LifecycleControllerInterface
  * 
  * @author matthieu.chaplin@sfr.com
- * @author pierre.cheynier@sfr.com
+ * @author pierre.cheynier@sfr.com 
  */
 public class DefaultLifecycleController implements LifecycleControllerInterface {
 
